2b67137a2657943f210b80a0884f7b44649ee129
Dude <michael_runschke@gmx.de>
2021-12-03 12:48:16 +0100

    getAge() / 2.Iteration / Call-Kaskade

--- src/com/cc/java/Cat.java
+++ src/com/cc/java/Cat.java
@@ -7,6 +7,8 @@ public class Cat {
     private int age;
     private boolean isFemale;
 
+    private int counter;
+
     public Cat(String name, String furColor, int age, boolean isFemale) {
         this.name = name;
         this.furColor = furColor;
@@ -23,16 +25,36 @@ public class Cat {
             default:
                 return "#!?";
         }
-
     }
 
     public String getAge() {
+        return checkCompliance();
+    
+    }
+
+    private String checkCompliance() {
         if (isFemale) {
-            return "This is an inappropriate Question!";
+            return checkEscalationLevel();
         } else {
             return Integer.toString(age);
         }
     }
 
+    private String checkEscalationLevel(){
+       
+        counter ++;
+
+        switch (counter) {           // Beim ersten mal case 1 , beim zweiten case 2, beim dritten case 3 , ab dem vierten default 
+            case 1:
+                return "This is an inappropriate Question!";
+            case 2:
+                return "I've told you once!";
+            case 3:
+                return "Talk to the hand!";
+            default: 
+                return "Piss off you retard!";
+        }
+    }
   
+
 }
